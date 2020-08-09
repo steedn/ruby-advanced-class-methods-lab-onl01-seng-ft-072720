ed1a253f1cc0e2aaec07284b589c23745a685682
Nathan Steed <steedn@outlook.com>
2020-08-09 04:01:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,16 @@ class Song
     @@all.find{|song| song.name == name}
   end
 
+  # def self.find_or_create_by_name(name)
+  #   if self.find_by_name(name) == true
+  #     self.find_by_name(name)
+  #   else
+  #     self.create_by_name(name)
+  #   end
+  # end
+
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == true
+    if self.find_by_name(name)
       self.find_by_name(name)
     else
       self.create_by_name(name)
