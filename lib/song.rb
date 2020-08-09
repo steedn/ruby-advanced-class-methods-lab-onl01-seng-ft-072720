2a40c86ed485239fe0b16a7599dbf9083ea58bd4
Nathan Steed <steedn@outlook.com>
2020-08-09 04:20:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,6 +62,10 @@ class Song
 
   def self.create_from_filename(filename)
     result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
   end
 
 
