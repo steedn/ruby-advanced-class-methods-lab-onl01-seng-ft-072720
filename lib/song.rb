19e95ed4dddb7ed497510d12b82dffa3d94b1b17
Nathan Steed <steedn@outlook.com>
2020-08-09 01:44:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
   def self.new_from_filename(filename)
     # binding.pry
     filename = filename.split(" - ")
-    filename = filename.split(".")
+    filename.split(".")
     filename.pop
     artist = self.artist_name(filename[0])
     song = self.name(filename[-1])
